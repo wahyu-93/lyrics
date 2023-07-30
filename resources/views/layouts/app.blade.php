dced9b1a92c8847fcc17a7be4c469b0ed4f6f042
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2023-07-30 15:18:33 +0800

    dinamic title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Lysrics') }}</title>
+    <title>@yield('title', 'lyrics')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
