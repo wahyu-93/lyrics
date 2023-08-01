759f675f1cb0ad549b533a1d5a3e97f165c09c05
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2023-08-02 04:57:40 +0800

    require message form album

--- resources/views/album/partials/form.blade.php
+++ resources/views/album/partials/form.blade.php
@@ -1,16 +1,23 @@
 <div class="form-group mb-3 ">
     <label for="name">Name Album</label>
-    <input type="text" name="name" id="name" class="form-control" value="{{ old('name') ?? $album->name }}" placeholder="Album (tahun)">
+    <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') ?? $album->name }}" placeholder="Album (tahun)">
+
+    @error('name')
+        <span class="text-danger">{{ $message }}</span>
+    @enderror
 </div>
 
 <div class="form-group mb-3">
     <label for="band">Band</label>
-    <select name="band" id="band" class="form-control">
+    <select name="band" id="band" class="form-control @error('band') is-invalid @enderror">
         <option disabled selected>Pilih Band</option>
         @foreach ($bands as $band)
             <option {{ $band->id === $album->band_id ? "selected" : "" }} value="{{ $band->id }}">{{ $band->name }}</option>
         @endforeach
     </select>
+    @error('band')
+        <span class="text-danger">{{ $message }}</span>
+    @enderror
 </div>
 
 <button type="submit" class="btn {{ $type }}">{{ $submit }}</button>
